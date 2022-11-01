0d3c9436baf6004ba29899e5b4bfb1da5645bd80
Oliver <olivermorgenstern1992@gmail.com>
2022-11-01 21:20:51 +0100

    + ethernet frame with destination address of ff:ff:ff:ff:ff:ff + changed destination address of arp request from ff:ff:ff:ff:ff:ff to 00:00:00:00:00:00 + handling of closing the handle + added some structure

--- src/main/java/me/nmt/Main.java
+++ src/main/java/me/nmt/Main.java
@@ -1,31 +1,56 @@
 package me.nmt;
 
 import org.pcap4j.core.PcapHandle;
+import org.pcap4j.core.PcapNativeException;
 import org.pcap4j.core.PcapNetworkInterface;
 import org.pcap4j.core.Pcaps;
 import org.pcap4j.packet.ArpPacket;
+import org.pcap4j.packet.EthernetPacket;
 import org.pcap4j.packet.Packet;
 import org.pcap4j.packet.namednumber.ArpHardwareType;
 import org.pcap4j.packet.namednumber.ArpOperation;
 import org.pcap4j.packet.namednumber.EtherType;
+import org.pcap4j.util.ByteArrays;
 import org.pcap4j.util.MacAddress;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    //region Core
+
+    public static void main(String[] args) throws UnknownHostException, SocketException, PcapNativeException {
+
+        //region Vars
+
+        InetAddress localAddress = InetAddress.getLocalHost();
+        NetworkInterface ni = NetworkInterface.getByInetAddress(localAddress);
+        MacAddress localMac = MacAddress.getByAddress(ni.getHardwareAddress());
+        PcapNetworkInterface nif = Pcaps.getDevByAddress(localAddress);
+        PcapHandle handle = nif.openLive(65536, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, 10000);
+
+        //endregion
+
+        //region Core
+
         try {
-            InetAddress localAddress = InetAddress.getLocalHost();
-            NetworkInterface ni = NetworkInterface.getByInetAddress(localAddress);
-            MacAddress localMac = MacAddress.getByAddress(ni.getHardwareAddress());
+
+            //region Getting IP-Address
+
             System.out.println("Detected local IPv4: " + localAddress);
             System.out.println("Detected local MAC address: " + localMac);
 
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please type in: IPv4 address!");
             String ipString = scanner.next();
+
+            //endregion
+
+            //region Check
 
             InetAddress address = InetAddress.getByName(ipString);
             if (address.isReachable(50)) {
@@ -35,37 +60,65 @@ public class Main {
                 return;
             }
 
+            //endregion
 
-            PcapNetworkInterface nif = Pcaps.getDevByAddress(localAddress);
+            //region ARP
 
-            PcapNetworkInterface.PromiscuousMode mode = PcapNetworkInterface.PromiscuousMode.PROMISCUOUS;
-
-            PcapHandle handle = nif.openLive(65536, mode, 10000);
-
-            ArpPacket.Builder sendPacket = new ArpPacket.Builder();
-            sendPacket
+            ArpPacket.Builder arpBuilder = new ArpPacket.Builder()
                     .dstProtocolAddr(address)
-                    .dstHardwareAddr(MacAddress.getByName("FF-FF-FF-FF-FF-FF", "-"))
+                    .dstHardwareAddr(MacAddress.getByName("00-00-00-00-00-00", "-"))
                     .srcProtocolAddr(localAddress)
                     .srcHardwareAddr(localMac)
-                    .protocolType(EtherType.ARP)
+                    .protocolType(EtherType.IPV4)
                     .hardwareType(ArpHardwareType.ETHERNET)
-                    .operation(ArpOperation.REQUEST);
+                    .operation(ArpOperation.REQUEST)
+                    .hardwareAddrLength((byte)MacAddress.SIZE_IN_BYTES)
+                    .protocolAddrLength((byte)ByteArrays.INET4_ADDRESS_SIZE_IN_BYTES);
 
-            handle.sendPacket(sendPacket.build());
+            //endregion
 
+            //region Ethernet
+
+            Packet packet = new EthernetPacket.Builder()
+                    .dstAddr(MacAddress.ETHER_BROADCAST_ADDRESS)
+                    .srcAddr(localMac)
+                    .type(EtherType.ARP)
+                    .payloadBuilder(arpBuilder)
+                    .paddingAtBuild(true)
+                    .build();
+
+            //endregion
+
+            //region Send and Receive
+
+            handle.sendPacket(packet);
             ArpPacket arp = (ArpPacket) searchPacket(handle, address);
 
-            handle.close();
+            //endregion
+
+            //region Result
 
             String mac = arp != null ? arp.getHeader().getSrcHardwareAddr().toString() : " / ";
-
             System.out.println("MAC of received ARP: " + mac);
             System.out.println("ARP: " + arp);
+
+            //endregion
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+        finally {
+            if (handle.isOpen()){
+                handle.close();
+            }
+        }
+
+        //endregion
     }
+
+    //endregion
+
+    //region Helper
 
     public static Packet searchPacket(PcapHandle handle, InetAddress address) {
         try {
@@ -89,4 +142,5 @@ public class Main {
         return null;
     }
 
+    //endregion
 }
